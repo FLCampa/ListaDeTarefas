dc9170f1348e467c08a4cdbc0010e0ae9050f7ca
Pedro Andrade <andrade.batista@aluno.ifsp.edu.br>
2021-07-11 21:39:30 -0300

    Adicionando o menu de layout e criação da intent para o cadastro de nova tarefa

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/listadetarefas/view/TarefasActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/listadetarefas/view/TarefasActivity.kt
@@ -1,8 +1,13 @@
 package br.edu.ifsp.scl.ads.pdm.listadetarefas.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.ads.pdm.listadetarefas.R
 import br.edu.ifsp.scl.ads.pdm.listadetarefas.adapter.OnClickListener
@@ -17,7 +22,7 @@ class TarefasActivity : AppCompatActivity(), OnClickListener {
     private lateinit var tarefasAdapter: TarefaAdapter
     private lateinit var tarefasLayoutManager: LinearLayoutManager
 
-
+    private lateinit var novaTarefaLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +49,38 @@ class TarefasActivity : AppCompatActivity(), OnClickListener {
         activityTarefasBinding.tarefasRv.layoutManager = tarefasLayoutManager
 
 
+        novaTarefaLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                val tarefa: Tarefa? = activityResult.data?.getParcelableExtra<Tarefa>(Intent.EXTRA_USER)
+                if (tarefa != null) {
+                    tarefasList.add(tarefa)
+                    tarefasAdapter.notifyDataSetChanged()
+
+
+                }
+            }
+        }
+
     }
 
     override fun onTarefaClick(posicao: Int) {
         val tarefa: Tarefa = tarefasList[posicao]
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.novaTarefaMi -> {
+            val novaTarefaIntent = Intent(this, CadastroActivity::class.java)
+            novaTarefaLauncher.launch(novaTarefaIntent)
+            true
+        }
+        else -> {
+            false
+        }
+    }
+
 }
